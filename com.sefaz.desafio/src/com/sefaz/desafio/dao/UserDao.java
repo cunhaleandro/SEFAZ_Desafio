ea3a484ffc140ced0d71a76ae2a1f30745ba5235
Leandro Cunha <leandro.jrc@hotmail.com>
2021-02-27 20:21:55 -0300

    Criação de relação com o banco de dados

---
+++ com.sefaz.desafio/src/com/sefaz/desafio/dao/UserDao.java
@@ -0,0 +1,107 @@
+package com.sefaz.desafio.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import com.sefaz.desafio.model.User;
+import com.sefaz.desafio.utils.HibernateUtil;
+
+public class UserDao {
+	
+	//Cria um novo usuário
+	
+	public void saveUser(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+  //Atualiza um usuário cadastrado
+	
+    public void updateUser(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+   //Deleta um usuário cadastrado
+    
+    public void deleteUser(int id) {
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            
+            if (user != null) {
+                session.delete(user);
+                System.out.println("user is deleted");
+            }
+            
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+//Get no usuário cadastrado pelo Id
+    
+    public User getUser(int id) {
+
+        Transaction transaction = null;
+        User user = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            user = session.get(User.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+ //Retorna todos os usuários cadastrados
+    
+    @SuppressWarnings("unchecked")
+    public List < User > getAllUser() {
+
+        Transaction transaction = null;
+        List < User > listOfUser = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            listOfUser = session.createQuery("from User").getResultList();
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return listOfUser;
+    }
+
+}
